f1f08d9bb9ff3e5a1baeb51c558e9cdebb031505
trevinshu <trevinshu2008@gmail.com>
2020-09-18 22:07:31 -0600

    final commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/* 
+ select the input element
+ store a reference to the input element in a variable
+ output the value  of your variable to the console.
+ set a value for the shipping rate per 1kg store it in a variable
+
+
+      object     .    method()
+      object    . property()
+      object   . event()
+
+    document.querySelector('css')
+
+    let =    allows you to overite the value
+    const = once only   Pie = 3.12
+ 
+ */
+
+//  reference variable
+// variable name   =   statement
+
+const selectInput = document.querySelector("input");
+console.log(selectInput);
